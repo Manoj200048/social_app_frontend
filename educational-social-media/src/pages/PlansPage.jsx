f2a63f009c0545cd46808c7f157002a9f5e7bec4
VilanICIEOS <buwanikaasinsala2000@gmail.com>
2025-05-11 16:40:10 +0530

    setup PlansPage component

---
+++ educational-social-media/src/pages/PlansPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Modal from "react-modal";
+import "./PlansPage.css";
+Modal.setAppElement("#root");
+
+const PlansPage = () => {
+  
+
+  return (
+    <div >
+      
+    </div>
+  );
+};
+
+export default PlansPage;
